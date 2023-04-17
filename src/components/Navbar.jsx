37a1f94b995f6d7b81bd7a7f5ed67f21085d23d2
madushanka061 <s17061@sci.pdn.ac.lk>
2023-04-17 18:24:06 +0530

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../App.css";
 import {
   AppBar,
   Button,
-  Grid,
   Tab,
   Tabs,
   Toolbar,
@@ -11,16 +11,16 @@ import {
   useMediaQuery,
   Badge,
   IconButton,
-  Stack,
   Avatar,
+  Tooltip,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import WbShadeRoundedIcon from "@mui/icons-material/WbShadeRounded";
 import { useState } from "react";
-import { Box } from "@mui/system";
-import Drawercomp from "./Drawercomp";
 import MailIcon from "@mui/icons-material/Mail";
 import PersonIcon from "@mui/icons-material/Person";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function notificationsLabel(count) {
   if (count === 0) {
@@ -32,21 +32,47 @@ function notificationsLabel(count) {
   return `${count} notifications`;
 }
 
-function Navbar({ links }) {
+function Navbar() {
   const navigate = useNavigate();
   const theme = useTheme();
-  console.log(theme);
+  // console.log(theme);
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
-  console.log(isMatch);
-  const [value, setValue] = useState();
 
-  const user = {
-    img: "https://images.unsplash.com/photo-1633534814099-685f19197c31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    alt: "avetar",
-    name: "Lizard",
-    description:
-      "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  const [value, setValue] = useState();
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const logOut = () => {
+    localStorage.removeItem("loggedUser");
+    navigate("/");
   };
+
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const logged = localStorage.getItem("loggedUser");
+    if (logged) {
+      setUser(JSON.parse(logged));
+    } else {
+      setUser(null);
+      // navigate("/");
+    }
+  }, []);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const settings = [
+    { name: "Profile", route: "/user-profile" },
+    { name: "My Jobs", route: "/my-jobs" },
+    { name: "Logout", route: "" },
+  ];
+  const links = [
+    { name: "Post job", route: "/job" },
+    { name: "Services", route: "/services" },
+    { name: "Contact Us", route: "/contact-us" },
+  ];
 
   return (
     <AppBar
@@ -59,10 +85,13 @@ function Navbar({ links }) {
       }}
     >
       <Toolbar sx={{ flexWrap: "wrap" }}>
-        <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-          <WbShadeRoundedIcon />
-          DOMESTICHUB
+        <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }} on>
+          <Link to="/" className="homelink">
+            <WbShadeRoundedIcon />
+            DOMESTICHUB
+          </Link>
         </Typography>
+
         <nav>
           <Tabs
             indicatorColor="secondary"
@@ -90,9 +119,39 @@ function Navbar({ links }) {
         </IconButton>
 
         {user && (
-          <Avatar sx={{ bgcolor: "black", marginLeft: 2, marginRight: 2 }}>
-            {user.img ? <img src={user.img} /> : <PersonIcon />}
-          </Avatar>
+          <>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  sx={{ bgcolor: "black", marginLeft: 2, marginRight: 2 }}
+                >
+                  {user.img ? <img src={user.img} /> : <PersonIcon />}
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting, index) => (
+                <MenuItem key={index} onClick={() => navigate(setting.route)}>
+                  <Typography textAlign="center">{setting.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </>
         )}
         {user ? (
           <Button
@@ -103,27 +162,42 @@ function Navbar({ links }) {
             }}
             variant="outlined"
             href="/"
-            onClick={() => console.log("LOGOUT")}
+            onClick={logOut}
           >
             LOGOUT
           </Button>
         ) : (
-          <Button
-            sx={{
-              marginLeft: 2,
-              marginRight: 2,
-              color: "white",
-              "&:hover": { backgroundColor: "rgba(149,157,162,1)" },
-            }}
-            variant="outlined"
-            href="/login"
-            onClick={() => console.log("LOGIN")}
-          >
-            LOGIN
-          </Button>
+          <>
+            <Link to="/login">
+              <Button
+                sx={{
+                  marginLeft: 2,
+                  marginRight: 2,
+                  color: "white",
+                  "&:hover": { backgroundColor: "rgba(149,157,162,1)" },
+                }}
+                variant="outlined"
+              >
+                LOGIN
+              </Button>
+            </Link>
+
+            <Button
+              sx={{
+                marginLeft: 2,
+                color: "white",
+                "&:hover": { backgroundColor: "rgba(149,157,162,1)" },
+                marginRight: 2,
+              }}
+              variant="outlined"
+              href="/sign-up"
+            >
+              SIGNUP
+            </Button>
+          </>
         )}
 
-        {user === null && (
+        {/* {user === null && (
           <Button
             sx={{
               marginLeft: 2,
@@ -132,10 +206,11 @@ function Navbar({ links }) {
               marginRight: 2,
             }}
             variant="outlined"
+            href="/sign-up"
           >
             SIGNUP
           </Button>
-        )}
+        )} */}
       </Toolbar>
     </AppBar>
   );
